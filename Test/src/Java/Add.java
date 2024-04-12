c7c5cc043e304d06a372b5739818b06fae6d24f3
Haha CORPORATION <Haha CORPORATION@VEDANT>
2024-04-12 11:34:00 +0530

    Master Commit

---
+++ Test/src/Java/Add.java
@@ -0,0 +1,13 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Add {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int a=scanner.nextInt();
+		int b=scanner.nextInt();
+		int c = a + b;
+		System.out.println(c);
+	}
+}
